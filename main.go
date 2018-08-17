0828b86528068362b9d8d779f7ea1773f6611f92
Nick Hale <nicholas.hale@gmail.com>
2018-08-17 14:44:45 +0100

    Integrated go-micro

--- main.go
+++ main.go
@@ -1,77 +1,73 @@
 package main
 
 import (
-    "log"
-    "net"
+    "fmt"
 
     pb "github.com/xzx1kf/consignment-service/proto/consignment"
+    micro "github.com/micro/go-micro"
     "golang.org/x/net/context"
-    "google.golang.org/grpc"
-    "google.golang.org/grpc/reflection"
 )
 
-const (
-    port = ":50051"
-)
-
-type IRepository interface {
+type Repository interface {
     Create(*pb.Consignment) (*pb.Consignment, error)
     GetAll() []*pb.Consignment
 }
 
-type Repository struct {
+type ConsignmentRepository struct {
     consignments []*pb.Consignment
 }
 
 // Create appends the new consignment to the repository of consignments.
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
     repo.consignments = append(repo.consignments, consignment)
     return consignment, nil
 }
 
-func (repo *Repository) GetAll() []*pb.Consignment {
+func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
     return repo.consignments
 }
 
 // service should implement all of the methods to satisfy the service
 // defined in the protobuf definition.
 type service struct {
-    repo IRepository
+    repo Repository
 }
 
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
     // Save the consignment
     consignment, err := s.repo.Create(req)
     if err != nil {
-        return nil, err
+        return err
     }
 
     // Return the matching 'Response' message created in the protobuf
     // definition.
-    return &pb.Response{Created: true, Consignment: consignment}, nil
+    res.Created = true
+    res.Consignment = consignment
+    return nil
 }
 
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
     consignments := s.repo.GetAll()
-    return &pb.Response{Consignments: consignments}, nil
+    res.Consignments = consignments
+    return nil
 }
 
 func main() {
-    repo := &Repository{}
+    repo := &ConsignmentRepository{}
 
-    // Set up the gRPC server.
-    lis, err := net.Listen("tcp", port)
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-    }
-    s := grpc.NewServer()
+    srv := micro.NewService(
+        micro.Name("go.micro.srv.consignment"),
+        micro.Version("latest"),
+    )
+
+    srv.Init()
 
     // Register the service with the gRPC server
-    pb.RegisterShippingServiceServer(s, &service{repo})
+    pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
 
-    // Register reflection service on gRPC server.
-    reflection.Register(s)
-    if err := s.Serve(lis); err != nil {
-        log.Fatalf("failed to serve: %v", err)
+    // Run the server
+    if err := srv.Run(); err != nil {
+        fmt.Println(err)
     }
 }
